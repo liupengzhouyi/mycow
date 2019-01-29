778ef0a014e098052c545214f78c00c974091774
liupeng <LIUPENG.0@outlook.com>
2019-01-29 21:22:12 +0800

    初始化

---
+++ src/main/java/cn/liupengstudy/mycow/MycowApplication.java
@@ -0,0 +1,14 @@
+package cn.liupengstudy.mycow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycowApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/cn/liupengstudy/mycow/MycowApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.liupengstudy.mycow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MycowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
